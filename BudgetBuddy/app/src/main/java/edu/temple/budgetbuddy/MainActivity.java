1ad64d9037ddc8b6ac1c8685eb5f4db91aed4d86
Genise <36108277+tug60038@users.noreply.github.com>
2019-10-25 19:57:48 -0400

    3 button go to their own activities

--- BudgetBuddy/app/src/main/java/edu/temple/budgetbuddy/MainActivity.java
+++ BudgetBuddy/app/src/main/java/edu/temple/budgetbuddy/MainActivity.java
@@ -1,11 +1,11 @@
 package edu.temple.budgetbuddy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +25,33 @@ public class MainActivity extends AppCompatActivity {
                 openCreateActivity();
             }
         });
-
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddExpenseActivity();
+            }
+        });
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewActivity();
+            }
+        });
 
     }
 
     public void openCreateActivity(){
         Intent intent = new Intent(this, createActivity.class);
+        startActivity(intent);
+    }
+
+    public void openAddExpenseActivity(){
+        Intent intent = new Intent(this, addExpenseActivity.class);
+        startActivity(intent);
+    }
+
+    public void openViewActivity(){
+        Intent intent = new Intent(this, viewActivity.class);
         startActivity(intent);
     }
 }
---
+++ BudgetBuddy/app/src/main/java/edu/temple/budgetbuddy/addExpenseActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.budgetbuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
