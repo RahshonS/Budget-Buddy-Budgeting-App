0982132e537a4939dbef2d284b9ee1d2880d1108
Genise <36108277+tug60038@users.noreply.github.com>
2019-10-29 18:14:12 -0400

    All Main Buttons Lead to Activities

---
+++ BudgetBuddy/app/build/generated/source/buildConfig/debug/edu/temple/budgetbuddy/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.temple.budgetbuddy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.temple.budgetbuddy";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
