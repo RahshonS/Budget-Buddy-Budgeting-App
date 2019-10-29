0bb7a40b2949722d62a2245fab0f9e077d6a7524
JoshuaKang1997 <joshkang1997@gmail.com>
2019-10-28 21:35:49 -0400

    added functionality for adding profiles and some for adding payment

--- app/src/main/java/edu/temple/budgetbuddy/MainActivity.java
+++ app/src/main/java/edu/temple/budgetbuddy/MainActivity.java
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> strList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] array = null;
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        if(getIntent().getStringExtra("profileName") == null){
+        }else{
+        strList.add(getIntent().getStringExtra("profileName"));
+        array = strList.toArray(new String[strList.size()]);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    R.layout.support_simple_spinner_dropdown_item, array);
+            spinner.setAdapter(adapter);
+        }
 
         Button newTemplate = (Button) findViewById(R.id.button4);
         Button payment = (Button) findViewById(R.id.button);
@@ -23,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getBaseContext(), createSavingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), payment_activity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/edu/temple/budgetbuddy/createSavingActivity.java
+++ app/src/main/java/edu/temple/budgetbuddy/createSavingActivity.java
@@ -2,6 +2,7 @@ package edu.temple.budgetbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.math.BigDecimal;
 
 public class createSavingActivity extends AppCompatActivity {
 
@@ -31,13 +34,23 @@ public class createSavingActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String income = incomeText.getText().toString();
+                Double dIncome = Double.parseDouble(income);
+                boolean fail = (BigDecimal.valueOf(dIncome).scale() > 2);
+
                 if (nameText.getText().toString().matches("") ||
                         incomeText.getText().toString().matches("") ||
                         spinner.getCount() == 0) {
                     Toast.makeText(createSavingActivity.this, "You left an empty field!", Toast.LENGTH_SHORT).show();
                 }
+                else if(fail){
+                    Toast.makeText(createSavingActivity.this, "Only 2 decimal places allowed in Monthly Income", Toast.LENGTH_SHORT).show();
+                }
                 else{
-                    Toast.makeText(createSavingActivity.this, "Successful Execution", Toast.LENGTH_SHORT).show();
+                    Intent mainIntent = new Intent(getBaseContext(),MainActivity.class);
+                    mainIntent.putExtra("profileName", nameText.getText().toString());
+                    startActivity(mainIntent);
                 }
             }
         });
