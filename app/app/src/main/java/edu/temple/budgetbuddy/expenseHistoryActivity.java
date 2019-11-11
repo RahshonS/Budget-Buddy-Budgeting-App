5b51b5e16451ee3ba268c8f63d4e7c4dc0995cb9
Zakia Hossain <zakia.hossain@temple.edu>
2019-11-11 17:43:54 -0500

    overwriting with files

---
+++ app/app/src/main/java/edu/temple/budgetbuddy/expenseHistoryActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.budgetbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class expenseHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_history);
+    }
+}
---
+++ app/app/src/main/java/edu/temple/budgetbuddy/payment_activity.java
@@ -0,0 +1,16 @@
+package edu.temple.budgetbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class payment_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_activity);
+
+
+    }
+}
